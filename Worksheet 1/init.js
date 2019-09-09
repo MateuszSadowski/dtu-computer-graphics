eadb7a0fd876f2fb6be3fd02a62003ac4560b5c4
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2019-09-09 16:57:58 +0200

    Add part 3, worksheet 1

--- Worksheet 1/init.js
+++ Worksheet 1/init.js
@@ -11,6 +11,8 @@ function init() {
 
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
+
+    //Set up data for vertex shader
     var vertices = [vec2(1.0, 0.0), vec2(1.0, 1.0), vec2(0.0, 0.0)];
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
@@ -18,8 +20,17 @@ function init() {
     var vPosition = gl.getAttribLocation(program, "a_Position");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-
-    gl.drawArrays(gl.POINTS, 0, vertices.length);
+    
+    //Set up data for fragment shader
+    var colors = [vec3(0.0, 1.0, 0.0), vec3(0.0, 0.0, 1.0), vec3(1.0, 0.0, 0.0)];
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    var vColor = gl.getAttribLocation(program, "a_Color");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+    
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length);
 }
 
 /**
