a91f9fce01f657380c9d86b782fe637010bf4957
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2019-09-09 17:33:35 +0200

    Add part 4, worksheet 1

--- Worksheet 1/init.js
+++ Worksheet 1/init.js
@@ -1,11 +1,17 @@
 onload = init;
 
+var gl;
+var vertices;
+
+var rotation = 0.0;
+var vRotation;
+
 function init() {
     var canvas = document.getElementById("c");
     canvas.width = 512;
     canvas.height = 512;
     // var gl = canvas.getContext("webgl");
-    var gl = setupWebGL(canvas);
+    gl = setupWebGL(canvas);
     gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -13,24 +19,28 @@ function init() {
     gl.useProgram(program);
 
     //Set up data for vertex shader
-    var vertices = [vec2(1.0, 0.0), vec2(1.0, 1.0), vec2(0.0, 0.0)];
+    vertices = [vec2(0.5, 0.0), vec2(0.0, 0.5), vec2(-0.5, 0.0), vec2(0.0, -0.5) ];
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
     var vPosition = gl.getAttribLocation(program, "a_Position");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-    
-    //Set up data for fragment shader
-    var colors = [vec3(0.0, 1.0, 0.0), vec3(0.0, 0.0, 1.0), vec3(1.0, 0.0, 0.0)];
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-    var vColor = gl.getAttribLocation(program, "a_Color");
-    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
-    
-    gl.drawArrays(gl.TRIANGLES, 0, vertices.length);
+
+    vRotation = gl.getUniformLocation(program, "u_Rotation");
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length);
+    render();
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    //Set rotation
+    rotation += 0.05;
+    gl.uniform1f(vRotation, rotation);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length);
+
+    requestAnimFrame(render);
 }
 
 /**
